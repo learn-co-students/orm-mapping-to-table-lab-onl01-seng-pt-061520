015b7e984f8c71b16b531f1cbe0b14172393d2ae
nickd <nickdiaz58@gmail.com>
2020-08-19 08:58:02 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,44 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-QUERY
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    QUERY
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students"
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-QUERY
+    INSERT INTO students(name, grade) VALUES (?, ?)
+    QUERY
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
