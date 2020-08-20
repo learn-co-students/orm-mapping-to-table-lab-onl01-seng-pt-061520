4e0586c685239ddfa2c34a46f96b6e1f958398fb
drewsview34 <drewsview34@outlook.com>
2020-08-19 18:44:56 -0700

    Created class atributes and initialize method

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn] 
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  
   
 end
