76557353f09bb12ff09f3c73f7c709dc3c015660
Cameron Hudson <camnn3@yahoo.com>
2020-09-01 03:00:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,9 +17,20 @@ class Student
     CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
-    album TEXT)
+    grade TEXT)
+    
     SQL
     DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES(?, ?)
+    
+    SQL
+    DB[:conn].execute(sql)
+    
   end
   
 end
