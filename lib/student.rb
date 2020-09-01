1c61d819ea67d77c15464a260cde1e2c6ccbd623
Cameron Hudson <camnn3@yahoo.com>
2020-09-01 02:54:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,4 +12,14 @@ class Student
     @id = id
   end
   
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    album TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
 end
